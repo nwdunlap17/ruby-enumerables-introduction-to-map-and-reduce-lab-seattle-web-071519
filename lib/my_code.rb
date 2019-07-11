605f44fde24eaa6d155736ab46ac22a73fa2ca07
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 18:32:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,49 @@
-# My Code here....
+def map_to_negativize(array)
+  array.size.times do |i|
+    array[i] *= -1
+  end
+  return array
+end
+
+def map_to_no_change(array)
+  return array
+end
+
+def map_to_double(array)
+  array.size.times do |i|
+    array[i] *= 2 
+  end
+  return array
+end
+
+def map_to_square(array)
+  array.size.times do |i|
+    array[i] *= array[i] 
+  end
+  return array
+end
+
+def reduce_to_total(array, start = 0)
+  array.size.times do |i|
+    start += array[i]
+  end
+  return start
+end
+
+def reduce_to_all_true(array)
+  array.size.times do |i|
+    if !!!array[i]
+      return false
+    end
+  end
+  return true
+end
+
+def reduce_to_any_true(array)
+  array.size.times do |i|
+    if !!array[i]
+      return true
+    end
+  end
+  return false
+end
